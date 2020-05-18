cc5195275e0cb9599c9f4eaf4556b61ce0f09742
Joris Bontje <jorisbontje@gmail.com>
2020-05-18 13:33:59 +0200

    fix png colors, remove hand colors

--- Scenes/PlayerHand.gd
+++ Scenes/PlayerHand.gd
@@ -14,12 +14,12 @@ func set_player_nr(new_player_nr):
   player_nr = new_player_nr
   $HBoxContainer/PlayerLabel.text = "Player " + str(player_nr)
   print("Setting player_nr to ", player_nr)
-  if player_nr == 1:
-    $CardRow.color = Color.aliceblue
-  elif player_nr == 2:
-    $CardRow.color = Color.aqua
-  else:
-    $CardRow.color = Color.black
+#  if player_nr == 1:
+#    $CardRow.color = Color.aliceblue
+#  elif player_nr == 2:
+#    $CardRow.color = Color.aqua
+#  else:
+#    $CardRow.color = Color.black
 
 func set_score(new_score):
   if score != new_score:
