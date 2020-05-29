09cd2de961b7a4e3d83115b94553b4db7697935c
Joris Bontje <jorisbontje@gmail.com>
2020-05-29 13:13:01 +0200

    instant vs ongoing duration label

--- Scenes/UnitCardView.gd
+++ Scenes/UnitCardView.gd
@@ -11,22 +11,27 @@ func _update_view():
     $StrengthLabel.text = str(card.strength)
     $AbilityLabel.text = card.get("ability", "")
     $"DescriptionBox/Description".text = card.get("description", "")
-    # TODO replace with icons
     if "duration" in card:
+      $"DescriptionBox/DurationLabel".visible = true
+      $"DescriptionBox/DurationLabel".text = card.duration
       if card.duration == "ongoing":
-        $AbilityLabel.text = "∞ " + $AbilityLabel.text
         $AbilityLabel.set("custom_colors/font_color", Color.white)
       elif card.duration == "instant":
-        $AbilityLabel.text = "! " + $AbilityLabel.text
         $AbilityLabel.set("custom_colors/font_color", Color.black)
+    else:
+      $"DescriptionBox/DurationLabel".visible = false
     $BackLabel.visible = false
     $DescriptionBox.visible = true
   else:
     $Background.color = Constants.THEATER_COLORS[Constants.THEATERS.NONE]
     $StrengthLabel.text = "2"
     $AbilityLabel.visible = false
+    $"DescriptionBox/DurationLabel".visible = false
     $BackLabel.visible = true
     $DescriptionBox.visible = false
+
+func _ready():
+  _update_view()
 
 func set_card(new_card):
   card = new_card
